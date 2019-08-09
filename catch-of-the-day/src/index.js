4287e86d3299ef024b1dcd5d73eb1875a3c65ef6
Robert <robert@Roberts-MacBook-Pro.local>
2019-08-09 07:53:06 -0600

    first component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import StorePiker from './components/StorePiker';
+
+render(<StorePiker />, document.querySelector('#main'));
+
