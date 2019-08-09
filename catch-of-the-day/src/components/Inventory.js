0c15431ed833f923c6283a327e47c987ef1444bd
Robert <robert@Roberts-MacBook-Pro.local>
2019-08-09 08:55:39 -0600

    added app

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+    render() {
+        return (
+            <div className="inventroy">
+                Inventory!!
+            </div>
+        );
+    }
+}
+
+export default Inventory;
