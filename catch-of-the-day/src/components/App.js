44de50c30014a06f84ad977a6d11007ddce34048
Robert <robert@Roberts-MacBook-Pro.local>
2019-08-10 08:20:36 -0600

    added edit form

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -39,6 +39,12 @@ class App extends React.Component {
         });
     };
 
+    updateFish = (key, updatedFish) => {
+        const fishes = { ...this.state.fishes };
+        fishes[key] = updatedFish;
+        this.setState({ fishes });
+    };
+
     loadSampleFishes = () => {
         this.setState({
             fishes
@@ -61,7 +67,7 @@ class App extends React.Component {
                     </ul>
                 </div>
                 <Order fishes={this.state.fishes} order={this.state.order} />
-                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} fishes={this.state.fishes} updateFish={this.updateFish} />
             </div>
         )
     }
---
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class EditFishForm extends React.Component {
+    handleChange = (event) => {
+        const updatedFish = { ...this.props.fish, [event.currentTarget.name]: event.currentTarget.value };
+        this.props.updateFish(this.props.index, updatedFish);
+    }
+    render() {
+        return (
+            <div className="fish-edit">
+                <input name="name" type="text" onChange={this.handleChange} value={this.props.fish.name} /> 
+                <input name="price"  type="text" onChange={this.handleChange} value={this.props.fish.price} />
+                <select name="status" onChange={this.handleChange} value={this.props.fish.status}>
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc" onChange={this.handleChange} value={this.props.fish.desc} />
+                <input name="image" type="text" onChange={this.handleChange} value={this.props.fish.image} />
+            </div>
+        )
+    }
+}
+
+export default EditFishForm;
